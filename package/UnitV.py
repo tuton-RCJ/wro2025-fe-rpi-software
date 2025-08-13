c7d49335221fff3fbdbff08eb5eb5cfb22c4e4e1
mitu24472 <mitus16@outlook.jp>
2025-08-14 03:08:48 +0900

    refactor UnitV class to initialize greens and reds as integers and update is_empty method

--- package/UnitV.py
+++ package/UnitV.py
@@ -4,8 +4,8 @@ class UnitV:
     def __init__(self,port="/dev/ttyUSB1"):
         self.ser = serial.Serial(port, 115200, timeout=1)
         self.ser.flush()
-        self.greens = []
-        self.reds = []
+        self.greens = 0
+        self.reds = 0
     
     def update_data(self):
         # 赤の中心座標(X), 緑の重心座標(X)
@@ -15,8 +15,9 @@ class UnitV:
         while self.ser.in_waiting > 1:
             self.ser.readline()
         ob = list(map(int,self.ser.readline().strip()))
+        self.greens = ob[0]
+        self.reds = ob[2]
         return ob[0], ob[2]
     
     def is_empty(self):
-        ud = self.update_data()
-        return all(ud, lambda x: x == 255)
+        return all(map(lambda x: x == 255, [self.greens, self.reds]))
