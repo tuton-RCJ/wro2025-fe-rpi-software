c8e02eaceffdbba38e6aa878fc38acaab7f4ad68
shuji <shuji.igarashi@gmail.com>
2025-08-14 14:29:55 +0900

    add wall-angle-estimate func

--- src/open.py
+++ src/open.py
@@ -28,7 +28,6 @@ def get_dist(angle):  # this function is expected to use after executing lidar.u
     return min_dist
 
 
-
 def get_side_dist():  # first is left, second is right
     return [get_dist(90), get_dist(-90)]
 
@@ -40,7 +39,8 @@ def decide_clockwise():  # decide turn direction based on side distances
     return direct
 
 
-def forward_to_specified_dist(dist: float): # the robot will move forward untill the front distance achieve given dist
+# the robot will move forward untill the front distance achieve given dist
+def forward_to_specified_dist(dist: float):
     sts.drive()
     while get_dist(0) > dist:
         lidar.update()
@@ -50,7 +50,7 @@ def forward_to_specified_dist(dist: float): # the robot will move forward untill
 def turn_corner():
     # forward_to_specified_dist(0.6)
     sts.drive(speed=30, angle=65*direct)
-    time.sleep(1) # need to adjust
+    time.sleep(1)  # need to adjust
     sts.stop()
 
 
@@ -91,10 +91,42 @@ def drive_straight():
     sts.drive()
 
 
+def estimate_wall_angle():  # return wall angle (°) [0~360)
+    lidar_data = []
+    calc_range = 30  # range of angles to consider(half)
+    for i in range(0, calc_range+1):
+        lidar_data.append((i, get_dist(i)))
+    for i in range(3600-calc_range, 360):
+        lidar_data.append((i, get_dist(i)))
+
+    data = np.array(lidar_data, dtype=float)
+    angles = np.deg2rad(data[:, 0])
+    dist = data[:, 1]
+
+    # convert to cartesian coordinates
+    x = dist * np.cos(angles)
+    y = dist * np.sin(angles)
+
+    x, y = y, x  # swap x and y to avoid m goes inf
+
+    # Least squares y = m*x + c
+    A = np.vstack([x, np.ones(len(x))]).T
+    m, _ = np.linalg.lstsq(A, y, rcond=None)[0]
+
+    # calc wall angle
+    line_angle = np.rad2deg(np.arctan(m))
+    wall_angle = line_angle  # need to adjust?
+    return wall_angle
+
+
 pid = PID_towall()
 
 
 if __name__ == "__main__":
+    
+    while True:
+        print(estimate_wall_angle())
+        time.sleep(0.5)
 
     turn_cnt = 0
     while turn_cnt < 12:
