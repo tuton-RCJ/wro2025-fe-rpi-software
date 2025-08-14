144592ef8dea3b910c6e4ca38c767191d3038a3a
shuji4649 <shuji.igarashi@gmail.com>
2025-08-14 16:13:34 +0900

    add left wall pid

--- src/open.py
+++ src/open.py
@@ -1,10 +1,12 @@
-from LiDAR import lidar_
-from STS3032 import sts3032
-from UnitV import UnitV
 import sys
 import time
 import numpy as np
+
 sys.path.append('/home/tuton/wro2025-fe-rpi-software/package')
+from LiDAR import lidar_
+from STS3032 import sts3032
+from UnitV import UnitV
+
 
 sts = sts3032()
 lidar = lidar_()
@@ -30,7 +32,7 @@ def get_dist(angle): # this function is expected to use after executing lidar.up
             min_dist = d[1]
             min_abs = abs(d[0] - rad_a)
             min_index = i
-    print(i, lidar.points[i])
+    # print(i, lidar.points[i])
     return min_dist
 
 
@@ -41,7 +43,7 @@ def get_side_dist():  # first is left, second is right
 def decide_clockwise():  # decide turn direction based on side distances
     global direct
     side_dists = get_side_dist()
-    direct = 1 if side_dists[0] > side_dists[1] else -1
+    direct = 1 if side_dists[0] < side_dists[1] else -1
     return direct
 
 
@@ -53,56 +55,12 @@ def forward_to_specified_dist(dist: float):
     sts.stop()
 
 
-def turn_corner():
-    # forward_to_specified_dist(0.6)
-    sts.drive(speed=30, angle=65*direct)
-    time.sleep(1)  # need to adjust
-    sts.stop()
-
-
-class PID_towall:
-    def __init__(self, target_distance=0.5):
-        self._kp = 2
-        self._ki = 5
-        self._kd = 0.1
-        self._k = 1
-        self._old_error = 0
-        self._integral = 0
-        self._Imax = 100
-        self._target_distance = target_distance
-
-    def update(self):
-        current_distance = get_dist(90 if direct == -1 else -90)
-
-        error = self._target_distance - current_distance
-
-        p = self._kp * error
-
-        self._integral += error
-        i = self._ki * self._integral
-        i = min(max(i, -self._Imax), self._Imax)
-
-        d = self._kd * (error - self._old_error)
-
-        pid = p + i + d
-        self._old_error = error
-        sts.drive(degree=pid*self._k)
-
-    def reset(self):
-        self._old_error = 0
-        self._integral = 0
-
-
-def drive_straight():
-    sts.drive()
-
-
 def estimate_wall_angle():  # return wall angle (°) [0~360)
     lidar_data = []
     calc_range = 30  # range of angles to consider(half)
-    for i in range(0, calc_range+1):
+    for i in range(0, calc_range+1,10):
         lidar_data.append((i, get_dist(i)))
-    for i in range(3600-calc_range, 360):
+    for i in range(3600-calc_range, 360,10):
         lidar_data.append((i, get_dist(i)))
 
     data = np.array(lidar_data, dtype=float)
@@ -125,23 +83,110 @@ def estimate_wall_angle():  # return wall angle (°) [0~360)
     return wall_angle
 
 
-pid = PID_towall()
+def estimate_left_wall_angle():  # return wall angle (°) [0~360)
+    lidar_data = []
+    calc_range = 20  # range of angles to consider(half)
+    for i in range(90, 90 + calc_range+1,10):
+        lidar_data.append((i-90, get_dist(i)))
+    for i in range(90-calc_range, 90,10):
+        lidar_data.append((i+270, get_dist(i)))
+
+    data = np.array(lidar_data, dtype=float)
+    angles = np.deg2rad(data[:, 0])
+    dist = data[:, 1]
+
+    # convert to cartesian coordinates
+    x = dist * np.cos(angles)
+    y = dist * np.sin(angles)
+
+    x, y = y, x  # swap x and y to avoid m goes inf
+
+    # Least squares y = m*x + c
+    A = np.vstack([x, np.ones(len(x))]).T
+    m, _ = np.linalg.lstsq(A, y, rcond=None)[0]
+
+    # calc wall angle
+    line_angle = np.rad2deg(np.arctan(m))
+    wall_angle = -line_angle  # need to adjust?
+    return wall_angle
+
+
+
+def turn_corner():
+    curve_angle = estimate_wall_angle()*direct
+    # forward_to_specified_dist(0.6)
+    sts.drive(speed=30, degree=65*direct)
+    time.sleep(2.8*((90+curve_angle)/90))  # need to adjust
+    sts.stop()
+
+
+class PID:
+    def __init__(self, target=0):
+        self._kp = 2
+        self._ki = 0
+        self._kd = 0.1
+        self._k = 1
+        self._old_error = 0
+        self._integral = 0
+        self._Imax = 100
+        self._target = target
+
+    def update(self,current):
+        
+        error = self._target - current
+
+        p = self._kp * error
+
+        self._integral += error
+        i = self._ki * self._integral
+        i = min(max(i, -self._Imax), self._Imax)
+
+        d = self._kd * (error - self._old_error)
+
+        pid = p + i + d
+        self._old_error = error
+        sts.drive(speed=90, degree=pid*self._k)
+
+    def reset(self):
+        self._old_error = 0
+        self._integral = 0
+
+
+def drive_straight():
+    pid.update(estimate_left_wall_angle())
+    # sts.drive(speed=80)
+
+pid=PID()
+
 
 
 if __name__ == "__main__":
     
-    while True:
-        print(estimate_wall_angle())
-        time.sleep(0.5)
+    try:
 
-    turn_cnt = 0
-    while turn_cnt < 12:
+        sts.stop()
+        # while True:
+        #     lidar.update()
+        #     print(estimate_wall_angle())
+        #     time.sleep(0.1)
         lidar.update()
-        drive_straight()
-        if get_dist(0) < 0.6:
-            turn_corner()
-            turn_cnt += 1
+        # decide_clockwise()
+        # turn_corner()
+        # time.sleep(1)
+        turn_cnt = 0
+        while turn_cnt < 8:
+            lidar.update()
+            drive_straight()
+            if get_dist(0) < 0.5:
+                decide_clockwise()
+                turn_corner()
+                turn_cnt += 1
 
-    forward_to_specified_dist(0.6)
-    lidar.turn_off()
-    sts.stop()
+        forward_to_specified_dist(0.6)
+        lidar.turn_off()
+        sts.stop()
+    except KeyboardInterrupt:
+        print("Ctrl+C pressed. Programm Stopped.")
+    finally:
+        lidar.turn_off()
+        sts.stop()
